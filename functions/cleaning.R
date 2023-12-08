7f346380af876de44d540a86ff12cfb5b345a540
p-number-8737 <150150878+p-number-8737@users.noreply.github.com>
2023-12-08 08:41:31 +0000

    Add files via upload

---
+++ functions/cleaning.R
@@ -0,0 +1,35 @@
+#the functions used to clean the raw penguins dataset
+
+
+# A function to make sure the column names are cleaned up, 
+# eg lower case and snake case
+clean_column_names <- function(penguins_data) {
+  penguins_data %>%
+    select(-starts_with("Delta")) %>%
+    select(-Comments) %>%
+    clean_names()
+}
+
+# A function to make sure the species names are shortened
+shorten_species <- function(penguins_data) {
+  penguins_data %>%
+    mutate(species = case_when(
+      species == "Adelie Penguin (Pygoscelis adeliae)" ~ "Adelie",
+      species == "Chinstrap penguin (Pygoscelis antarctica)" ~ "Chinstrap",
+      species == "Gentoo penguin (Pygoscelis papua)" ~ "Gentoo"
+    ))
+}
+
+# A function to subset the data based on the list of column names
+subset_columns <- function(penguins_data, column_names) {
+  penguins_data %>%
+    select(all_of(column_names))
+}
+
+
+# A function to remove rows which contain NA values
+remove_NA <- function(penguins_data) {
+  penguins_data %>%
+    na.omit()
+}
+
